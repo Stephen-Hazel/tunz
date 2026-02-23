7299765120790f2b45f5526bc839a5d40e3d84fc
Stephen Hazel <stephen.hazel@gmail.com>
2026-02-22 17:44:24 -0800

    gettin close :)

--- app/src/main/java/app/shaz/tunz/MainActivity.kt
+++ app/src/main/java/app/shaz/tunz/MainActivity.kt
@@ -2,39 +2,32 @@ package app.shaz.tunz
 
 import android.os.Environment
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import app.shaz.tunz.databinding.ActivityMainBinding
 import android.util.Log
 import java.io.File
 import android.media.MediaPlayer
-import android.view.Menu
-import android.view.MenuItem
-/*
-import androidx.constraintlayout.widget.ConstraintLayout
-import android.view.ViewGroup
+import android.view.View
 import android.widget.CheckBox
+import android.widget.LinearLayout
+import android.widget.TableRow
 import android.widget.TextView
- */
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.google.android.material.snackbar.Snackbar
-import app.shaz.tunz.databinding.ActivityMainBinding
 
 data class FNList (
    val dir: String,
    var fn:  MutableList<String>
 )
 
+
 class MainActivity: AppCompatActivity ()
-{  private lateinit var appBarCfg: AppBarConfiguration
-   private lateinit var binding:   ActivityMainBinding
+{  private lateinit var b: ActivityMainBinding
 
    private var mplay: MediaPlayer? = null
    private var path = ""
    private var mp3  = mutableListOf<FNList>()
 
-   private var rand: Boolean = true
+   private var shuf: Boolean = true
    private var pick = mutableListOf<String>()
    private var done = mutableListOf<String>()
 
@@ -43,18 +36,16 @@ class MainActivity: AppCompatActivity ()
    private var ppos = 0
 
    fun next ()
-   {
-      Log.d ("Files", "next start")
-      mplay?.stop ()
+   {  mplay?.stop ()
       mplay?.reset ()
-      Log.d ("Files", "song done")
+Log.d ("Files", "song done")
       done.add (song)
+      play.removeAt (ppos)
+      b.loTbl.removeViews (ppos, 1)
 
-      Log.d ("Files", "bump ppos")
-      ppos++
       if (ppos < play.size) {
          song = play [ppos]
-         Log.d ("Files", "ppos=$ppos fn=$song")
+Log.d ("Files", "ppos=$ppos fn=$song")
          mplay?.setDataSource ("$path/$song")
          mplay?.prepare ()
          mplay?.start ()
@@ -62,36 +53,50 @@ class MainActivity: AppCompatActivity ()
       }
    }
 
-   fun rePlay ()
-   // use rand and pick to make playlist from mp3
+   fun rePlay (ac: MainActivity)
+   // use shuf, pick, and done to make playlist from mp3
    {
-      Log.d ("Files", "rePlay start")
+Log.d ("Files", "rePlay start")
+      pick.forEach { Log.d("Files", "pick=$it") }
+
       if (mplay?.isPlaying == true)  mplay?.stop ()
       mplay?.reset ()
 
       play = mutableListOf ()
-      pick.forEach { p ->
-         mp3.forEach { m ->
-            if (p == m.dir)  m.fn.forEach { fn ->
-               play.add ("$p/$fn")
+      b.loTbl.removeAllViews ()
+      if (pick.isEmpty ())  return
+
+      pick.forEach { p -> mp3.forEach { m ->
+         if (p == m.dir)  m.fn.forEach { fn -> play.add ("$p/$fn") }
+      }}
+      if (shuf) {
+         play.removeAll (done)
+         if (play.isEmpty ()) {
+            done = mutableListOf ()
+
+         // one more time sigh
+            pick.forEach { p ->
+               mp3.forEach { m ->
+                  if (p == m.dir)  m.fn.forEach { fn ->
+                     play.add ("$p/$fn")
+                  }
+               }
             }
          }
-      }
-      if (rand) {
-         play.removeAll (done)
          play.shuffle ()
       }
       ppos = 0
-//      Log.d ("Files", "gonna set textview")
-//     val tv: TextView = findViewById (R.id.textview_second)
-//      Log.d ("Files", "a")
-//      tv.text = play.joinToString (separator = "\n")
-//      Log.d ("Files", "b")
-
-       if (play.isEmpty ())  return
+      play.forEach { fn ->
+        val tr = TableRow (ac)
+        val tv = TextView (ac)
+         tv.text = fn
+         tr.addView (tv)
+         b.loTbl.addView (tr)
+      }
 
       song = play [ppos]
       Log.d ("Files", "gonna play $song")
+
       mplay?.setDataSource ("$path/$song")
       mplay?.prepare ()
       mplay?.start ()
@@ -100,81 +105,73 @@ class MainActivity: AppCompatActivity ()
 
    override fun onCreate (state: Bundle?)
    {  super.onCreate (state)
-      binding = ActivityMainBinding.inflate (layoutInflater)
-      setContentView      (binding.root)
-      setSupportActionBar (binding.toolbar)
-     val nav = findNavController (R.id.nav_host_fragment_content_main)
-      appBarCfg = AppBarConfiguration (nav.graph)
-      setupActionBarWithNavController (nav, appBarCfg)
-      binding.fab.setOnClickListener { view ->
-         Snackbar.make (view, "...your action", Snackbar.LENGTH_LONG)
-            .setAction ("Action", null).setAnchorView (R.id.fab).show ()
-      }
+      b = ActivityMainBinding.inflate (layoutInflater)
+      setContentView (b.root)
 
-   // open mediaplayer
       mplay = MediaPlayer ()
 
    // load persisted vars
      val p = getSharedPreferences ("prf", MODE_PRIVATE)
-      rand = p.getBoolean   ("rand", true)
+      shuf = p.getBoolean   ("shuf", true)
       pick = p.getStringSet ("pick",
-                             emptySet ())?.toMutableList () ?: mutableListOf ()
+             emptySet ())?.toMutableList () ?: mutableListOf ()
       done = p.getStringSet ("done",
-                             emptySet ())?.toMutableList () ?: mutableListOf ()
-//    done.forEach { Log.d("Files", "done=$it") }
+             emptySet ())?.toMutableList () ?: mutableListOf ()
+      done.forEach { Log.d("Files", "done=$it") }
+      pick.forEach { Log.d("Files", "pick=$it") }
 
    // list off Music into path, mp3
       path = Environment.getExternalStorageDirectory ().toString () + "/Music"
      val mus = File (path).listFiles ()
-      Log.d("Files", path + ' ' + mus!!.size)
+     val dir = mutableListOf<String> ()
       for (i in mus.indices) {
          if (! mus [i]!!.isDirectory ())  continue
-        val dr = mus [i]!!.getName ()
-         if (dr == ".thumbnails")         continue
-
-        val d2 = File ("$path/$dr").listFiles ()
-         Log.d("Files", "mus dir=$dr files=${d2!!.size}")
-        val ls: Array<String> = d2.map { it.getName () }.toTypedArray ()
-         ls.sort ()
-         mp3.add (FNList (dr, ls.toMutableList ()))
-
-
-         if ((dr == "An") || (dr == "_a"))  pick.add (dr)
+        val n = mus [i]!!.getName ()
+         if (n == ".thumbnails")  continue
+         dir.add (n)
       }
-
-      rePlay ()
+      dir.sort ()
+      dir.forEach { d ->
+        val dl = File ("$path/$d").listFiles ()
+         Log.d("Files", "mus dir=$d files=${dl!!.size}")
+        val ls: Array<String> = dl.map { it.getName () }.toTypedArray ()
+         ls.sort ()
+         mp3.add (FNList (d, ls.toMutableList ()))
+      }
 
    // init gui
-/*
-     val lo = findViewById<ConstraintLayout>(R.id.pickLayout)
-     val cb = CheckBox (this)
-      cb.text = "rand"
-      cb.layoutParams = ConstraintLayout.LayoutParams (
-         ViewGroup.LayoutParams.WRAP_CONTENT,
-         ViewGroup.LayoutParams.WRAP_CONTENT
-      )
-      cb.setOnCheckedChangeListener { cb, chk ->
-         rand = chk
-         rePlay ()
+      b.cbShuf.setChecked (shuf)
+      b.cbShuf.setOnCheckedChangeListener { _, chk ->
+         shuf = chk
+         rePlay (this)
       }
-      lo.addView (cb)
 
-      mp3.forEach { d ->
+      mp3.forEach { m ->
         val cb = CheckBox (this)
-         cb.text = d.dir
-         cb.layoutParams = ConstraintLayout.LayoutParams (
-            ViewGroup.LayoutParams.WRAP_CONTENT,
-            ViewGroup.LayoutParams.WRAP_CONTENT
-         )
+         cb.text = m.dir
+         cb.id = View.generateViewId ()
+         cb.setChecked (pick.contains (m.dir))
+         cb.layoutParams = LinearLayout.LayoutParams (
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT)
          cb.setOnCheckedChangeListener { cb, chk ->
-            if (chk) pick.add    (cb.text.toString ())
-            else     pick.remove (cb.text.toString ())
-            rePlay ()
+            if (chk)  pick.add    (cb.text as String)
+            else      pick.remove (cb.text as String)
+            rePlay (this)
          }
-         lo.addView (cb)
+         b.loLin.addView (cb)
       }
-*/
+
+      b.fab.setOnClickListener { view ->
+         Snackbar.make (view, "play/pause", Snackbar.LENGTH_LONG)
+                 .setAction ("Action", null).setAnchorView (R.id.fab).show ()
+         if (mplay?.isPlaying == true)  mplay?.pause ()
+         else                           mplay?.start ()
+      }
+
+      rePlay (this)
    }
+
 
    override fun onDestroy ()
    {  super.onDestroy ()
@@ -185,31 +182,11 @@ class MainActivity: AppCompatActivity ()
       mplay = null
 
    // persist vars
+      Log.d("Files", "save pref start")
+      pick.forEach { Log.d("Files", "pick=$it") }
      val e = getSharedPreferences ("prf", MODE_PRIVATE).edit ()
-      e.putBoolean   ("rand", rand).apply ()
-      e.putStringSet ("pick", pick.toSet ()).apply ()
-      e.putStringSet ("done", done.toSet ()).apply ()
-   }
-
-
-   override fun onCreateOptionsMenu (menu: Menu): Boolean
-   // Inflate the menu; this adds items to the action bar if it is present.
-   {  menuInflater.inflate (R.menu.menu_main, menu)
-      return true
-   }
-
-   override fun onOptionsItemSelected (item: MenuItem): Boolean
-   // Handle action bar item clicks here. The action bar will
-   // automatically handle clicks on the Home/Up button, so long
-   // as you specify a parent activity in AndroidManifest.xml.
-   {  return when (item.itemId) {
-         R.id.action_settings -> true
-         else -> super.onOptionsItemSelected (item)
-      }
-   }
-
-   override fun onSupportNavigateUp (): Boolean
-   {  return findNavController (R.id.nav_host_fragment_content_main)
-                .navigateUp (appBarCfg) || super.onSupportNavigateUp ()
+      e.putBoolean   ("shuf", shuf).commit ()
+      e.putStringSet ("pick", pick.toSet ()).commit ()
+      e.putStringSet ("done", done.toSet ()).commit ()
    }
 }
