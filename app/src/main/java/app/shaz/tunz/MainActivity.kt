407eb7fd65df5f9dc6ca55d87ebe52fcc563fab3
Stephen Hazel <stephen.hazel@gmail.com>
2026-02-27 18:35:12 -0800

    i think its dooooooooone :)

--- app/src/main/java/app/shaz/tunz/MainActivity.kt
+++ app/src/main/java/app/shaz/tunz/MainActivity.kt
@@ -1,7 +1,12 @@
 package app.shaz.tunz
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.graphics.Color
 import android.graphics.Typeface
+import android.media.AudioManager
 import android.os.Environment
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -18,6 +23,7 @@ import android.widget.CheckBox
 import android.widget.LinearLayout
 import android.widget.TableRow
 import android.widget.TextView
+
 
 data class FNList (
    val dir: String,
@@ -41,6 +47,17 @@ class MainActivity: AppCompatActivity ()
    private var ppos = 0
    private var selRow: TableRow? = null
 
+   private lateinit var btDisco: BTDisco
+   private val intentFilter = IntentFilter (
+                                       AudioManager.ACTION_AUDIO_BECOMING_NOISY)
+
+   private class BTDisco (private val mp: MediaPlayer): BroadcastReceiver () {
+      override fun onReceive (context: Context, intent: Intent)
+      {  if (intent.action == AudioManager.ACTION_AUDIO_BECOMING_NOISY)
+            if (mp.isPlaying) mp.pause ()
+      }
+   }
+
 
    fun substr (s: String, b: Int, l: Int): String     // good grief
    {  return s.substring (b, b+l);  }
@@ -48,36 +65,33 @@ class MainActivity: AppCompatActivity ()
 
    fun fmtfn (fn: String): SpannableString
    { var s = fn
-Log.d("Files", "fn=$fn")
      val d = s.substringBefore ("/")   // dir
       s =    s.substringAfter  ("/")
       s = substr (s, 0, s.length-4)    // toss dir/ and .mp3
       s = s.replace ("_", " ")         // _ => space
-     val f = s.indexOf     ("-")
+     val f = s.indexOf     ("-")       // group - year album song# - title
      val l = s.lastIndexOf ("-")
-Log.d("Files", "   f=$f l=$l")
      var g: String
      var t: String
      var x: String
       if (f != -1) {                   // l must have been set too
-         g = substr (fn, 0, f)
-         t = fn.substring (l+1)
+         g = substr (s, 0, f)
+         t = s.substring (l+1)
          if (f == l)  x = ""
          else         x = substr (s, f+1, l-f-1)
       }
       else {
-         g = "??"
-         t = fn
+         g = ""
+         t = s
          x = ""
       }
-     val ss = SpannableString ("$t $g $d $x")
+     val ss = SpannableString ("$g  $t  $x  $d")
      var b  = ss.indexOf (t)
       if (b != -1)  ss.setSpan (StyleSpan (Typeface.BOLD), b, b + t.length,
                                 Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
       b = ss.indexOf (d)
       if (b != -1)  ss.setSpan (StyleSpan (Typeface.BOLD), b, b + d.length,
                                 Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-Log.d("Files", "   ss=$ss")
       return ss
    }
 
@@ -85,7 +99,7 @@ Log.d("Files", "   ss=$ss")
    fun next (row: Int = -1)
    {  mplay?.stop ()
       mplay?.reset ()
-Log.d ("Files", "song done row=$row")
+//Log.d ("Files", "song done row=$row")
       if (row == -1) {
          done.add (song)
          play.removeAt (ppos)
@@ -104,7 +118,7 @@ Log.d ("Files", "song done row=$row")
 
       if (ppos < play.size) {
          song = play [ppos]
-Log.d ("Files", "ppos=$ppos fn=$song")
+//Log.d ("Files", "ppos=$ppos fn=$song")
          mplay?.setDataSource ("$path/$song")
          mplay?.prepare ()
          mplay?.start ()
@@ -115,9 +129,8 @@ Log.d ("Files", "ppos=$ppos fn=$song")
    fun rePlay (ac: MainActivity)
    // use shuf, pick, and done to make playlist from mp3
    {
-Log.d ("Files", "rePlay start")
-      pick.forEach { Log.d("Files", "pick=$it") }
-
+//Log.d ("Files", "rePlay start")
+//pick.forEach { Log.d("Files", "pick=$it") }
       if (mplay?.isPlaying == true)  mplay?.stop ()
       mplay?.reset ()
 
@@ -172,7 +185,7 @@ Log.d ("Files", "rePlay start")
       }
 
       song = play [ppos]
-Log.d ("Files", "gonna play $song")
+//Log.d ("Files", "gonna play $song")
 
       mplay?.setDataSource ("$path/$song")
       mplay?.prepare ()
@@ -195,8 +208,8 @@ Log.d ("Files", "gonna play $song")
              emptySet ())?.toMutableList () ?: mutableListOf ()
       done = p.getStringSet ("done",
              emptySet ())?.toMutableList () ?: mutableListOf ()
-done.forEach { Log.d("Files", "done=$it") }
-pick.forEach { Log.d("Files", "pick=$it") }
+//done.forEach { Log.d("Files", "done=$it") }
+//pick.forEach { Log.d("Files", "pick=$it") }
 
    // list off Music into path, mp3
       path = Environment.getExternalStorageDirectory ().toString () + "/Music"
@@ -211,7 +224,7 @@ pick.forEach { Log.d("Files", "pick=$it") }
       dir.sort ()
       dir.forEach { d ->
         val dl = File ("$path/$d").listFiles ()
-         Log.d("Files", "mus dir=$d files=${dl!!.size}")
+//Log.d("Files", "mus dir=$d files=${dl!!.size}")
         val ls: Array<String> = dl.map { it.getName () }.toTypedArray ()
          ls.sort ()
          mp3.add (FNList (d, ls.toMutableList ()))
@@ -247,6 +260,9 @@ pick.forEach { Log.d("Files", "pick=$it") }
          else                           mplay?.start ()
       }
 
+      btDisco = BTDisco (mplay!!)
+      registerReceiver (btDisco, intentFilter)
+
       rePlay (this)
    }
 
@@ -259,9 +275,11 @@ pick.forEach { Log.d("Files", "pick=$it") }
       mplay?.release ()
       mplay = null
 
+      unregisterReceiver (btDisco)
+
    // persist vars
-Log.d("Files", "save pref start")
-pick.forEach { Log.d("Files", "pick=$it") }
+//Log.d("Files", "save pref start")
+//pick.forEach { Log.d("Files", "pick=$it") }
      val e = getSharedPreferences ("prf", MODE_PRIVATE).edit ()
       e.putBoolean   ("shuf", shuf).commit ()
       e.putStringSet ("pick", pick.toSet ()).commit ()
